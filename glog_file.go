18fd967fd4cceae18a186b579115e81b0e20daa1
root <xinmin.he@yunzhanghu.com>
2019-06-26 13:22:47 +0800

    rename parameter for gray test

--- glog_file.go
+++ glog_file.go
@@ -38,14 +38,14 @@ var logDirs []string
 
 // If non-empty, overrides the choice of directory in which to write logs.
 // See createLogDirs for the full list of possible destinations.
-var logDir = flag.String("log_dir", "", "If non-empty, write log files in this directory")
+var glogDir = flag.String("glog_dir", "", "If non-empty, write log files in this directory")
 
 var LogRotateInterval = flag.String("rotate_interval", "day",
 	"Set the rolling log intervals to be months, days, hours, and minutes, and values correspond to 'month', 'day', 'hour', 'minute' respectively")
 
 func createLogDirs() {
-	if *logDir != "" {
-		logDirs = append(logDirs, *logDir)
+	if *glogDir != "" {
+		logDirs = append(logDirs, *glogDir)
 	} else {
 		logDirs = append(logDirs, os.TempDir())
 	}
